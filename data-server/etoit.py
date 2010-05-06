ce244ff609de298070b6845c0d4a3a9de3f33299
Supergonzalo <gonzalo.na@gmail.com>
2010-05-06 12:26:22 +0100

    Agrega libreria rsm que crea archivos cache (station.rsm) para cada directorio

--- data-server/etoit.py
+++ data-server/etoit.py
@@ -14,7 +14,7 @@ import pywapi
 import pprint
 from metar import Metar
 import etowind20
-
+from rsm import generate_rsm
 
 
 #Gets Weather Reports from stations given as a parameter (argv) and stores them in files
@@ -116,7 +116,7 @@ def etoit(stationcode,dayofyear):
 				observation=arch(dayofyear+str(i), 'r',0)
 				if len(observation[1])>5 or len(observation[3])>5: #there's data in the file
 					info[i]="\nTimestamp: "+str(i)+ etowind20.etowind(observation,station)
-		rsm=dayofyear[0:4]
+		
 		dex=dayofyear+'.dex'
 		if os.path.isfile(dex):
 			os.remove(dex)			
@@ -126,11 +126,8 @@ def etoit(stationcode,dayofyear):
 		
 		if not len(dexinfo)==0:
 			arch(dex,'a',etrad10.etorad(dexinfo,station,dex))	#writes timestamp
-			if os.path.isfile(rsm):
-				mode='a'
-			else:
-				mode='w'
-			#generate_rsm(dayofyear,dexinfo,station)
+
+		#rsm.addto_rsm(station,dexinfo)
 				
 	else:
 		print "No data for station"
---
+++ data-server/rsm.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+from __future__ import with_statement
+import os, re
+
+
+def addto_rsm(station,dexinfo):						#Adds dexinfo to the top of station.rsm, if file does not exist it creates it
+	#print 'dexinfo#####################\n'
+	#print dexinfo
+	rsm=station[0:4]+'.rsm'
+	temp=''
+	if os.path.isfile(rsm):
+		f=open(rsm,'r')
+		temp=f.readlines()
+		f.close()
+	f=open(rsm,'w')
+	f.writelines(dexinfo)
+	f.writelines(temp)
+	f.close()
+
+def create_rsm(station):			#Creates .rsm file for a directory '.', if exists deletes the previous one
+	date_file_list=[]
+	rsm=station[0:4]+'.rsm'
+#Reindex .dex files, delete old rsm file
+	if os.path.isfile(rsm):
+		f=open(rsm,'w')
+		f.write('')
+		f.close()
+	for file in glob.glob('*.dex'):				#reverse order
+		date_file_list.append(file)
+	date_file_list.sort()
+	#date_file_list.reverse() 							# newest mod date now first
+	for element in date_file_list:
+		eto=element+'\n'
+		with open(element, "r") as f:
+			f.seek (0, 2)           					# Seek @ EOF
+			fsize = f.tell()       					 	# Get Size
+			f.seek (max (fsize-1024, 0), 0) 	# Set pos @ last n chars
+			lines = f.readlines()       			# Read to end
+		lines = lines[-10:]    							# Get last 10 lines
+		findstr=['ETO WindTODAY','ETOradTODAY','ETOTODAY','Estimada','TempMax','TempMin','TempMed','WindMed']
+		for line in lines:
+			for item in findstr:
+				if item in line:
+					eto=eto+line
+			
+			#print element
+			#print eto
+		addto_rsm(station,eto+'\n')	#Write footer to .rsm
+				
+
+
+
